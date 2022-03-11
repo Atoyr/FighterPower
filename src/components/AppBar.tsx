4dfea9b93e6c9bcae7b4473e9cb674882018461f
r.uchiyama <lessironglance@gmail.com>
2022-03-11 10:53:47 +0900

    update appbar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -16,7 +16,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import Drawer from '@mui/material/Drawer';
-import { useLocation, useNavigate, Outlet, } from "react-router-dom";
+import { useNavigate, Outlet, Link } from "react-router-dom";
 import { useAuthContext } from 'context/AuthProvider';
 import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
 import SiteLogo from './SiteLogo';
@@ -73,56 +73,86 @@ const ResponsiveAppBar = () => {
               sx={{ width: 250}}
               role="presentation"
             >
-      <List>
-          <ListItem button >
-            <ListItemText primary={"foo"} />
-          </ListItem>
-      </List>
-    </Box>
+        <List>
+            <ListItem button >
+              <ListItemText primary={"foo"} />
+            </ListItem>
+        </List>
+      </Box>
           </Drawer>
         </Box>
     );
   };
 
   const CenterArea = () => {
+    const to = auth.authState.user == null ? "/" : "home";
     return (
       <Box sx={{flexGrow: 1, px: 0.5, display: 'flex', flexDirection: 'row' }}>
-        <SiteLogo size={36} />
+        <Link to={to} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          <SiteLogo size={36} />
+        </Link>
       </Box>
     );
   };
 
   const RightArea = () => {
-    return(
-      <Box sx={{ flexGrow: 0 , px: 0.5}}>
-        <Tooltip title="Open settings">
-          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-          </IconButton>
-        </Tooltip>
-        <Menu
-          sx={{ mt: '45px' }}
-          id="menu-appbar"
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          open={false}
-          onClose={handleCloseUserMenu}
-        >
-          {settings.map((setting) => (
-            <MenuItem key={setting} onClick={handleCloseUserMenu}>
-              <Typography textAlign="center">{setting}</Typography>
-            </MenuItem>
-          ))}
-        </Menu>
-      </Box>
-      );
+
+    if ( auth.authState.user == null)
+    {
+      return(
+        <Box 
+          sx={{ 
+            flexGrow: 0 ,
+            display: 'flex',
+            flexDirection: 'row',
+            borderRadius: 1,
+            }}>
+          <Button
+            key='signin'
+            onClick={() => navigate('/signin')}
+            sx={{ my: 2, color: 'white', display: 'block', border:0 }}>
+            SIGN IN
+          </Button>
+          <Button
+            key='signin'
+            onClick={() => navigate('/signup')}
+            sx={{ my: 2, color: 'white', display: 'block', border:1, m: 2 }}>
+            SIGN UP
+          </Button>
+        </Box>
+        );
+    } else {
+      return(
+        <Box sx={{ flexGrow: 0 , px: 0.5}}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={false}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+        );
+    }
   };
 
   if ( auth.authState.user != null)
