44e1c1c85c8b5d3882c3c835494fb12cc1e5f21d
r.uchiyama <lessironglance@gmail.com>
2022-03-14 10:33:41 +0900

    add trycatch function

--- src/data/result.ts
+++ src/data/result.ts
@@ -1,23 +1,23 @@
-export type Result<T, E> = Success<T, E> | Failure<T, E>
+export type Result<T, E extends Error> = Success<T> | Failure<E>
 
-export class Success<T, E> {
+export class Success<T> {
   constructor(readonly value: T) {}
   __type = 'success'
-  isSuccess(): this is Success<T, E> {
+  isSuccess(): this is Success<T> {
     return true
   }
-  isFailure(): this is Failure<T, E> {
+  isFailure(): this is Failure<Error> {
     return false
   }
 }
 
-export class Failure<T, E> {
+export class Failure<E extends Error> {
   constructor(readonly value: E) {}
   __type = 'failure'
-  isSuccess(): this is Success<T, E> {
+  isSuccess(): this is Success<unknown> {
     return false
   }
-  isFailure(): this is Failure<T, E> {
+  isFailure(): this is Failure<E> {
     return true
   }
 }
