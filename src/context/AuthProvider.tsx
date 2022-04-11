41d8beacfc4fcf8ae1fbb67c780bcad8628400c2
RyotaUchiyama <lessironglance@gmail.com>
2022-04-12 08:50:49 +0900

    update action to function

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -30,100 +30,99 @@ export interface AuthContextType {
 
 let AuthContext = React.createContext<AuthContextType>(null!);
 
-function AuthProvider({ children }: { children: React.ReactNode }) {
-  let authState = useAuthState();
-
-  let signup = (param: AuthParameter, callback: (user: UserCredential) => void, errorCallback: (e : Error) => void) => {
-    let promise : Promise<UserCredential> | null = null;
-    switch (param.AuthType) {
-      case "EmailAndPassword":
-        promise = createUserWithEmailAndPassword(firebaseAuth, param.email as string, param.password as string);
-        break
-      case "GoogleAuth":
+function signup(param: AuthParameter, callback: (user: UserCredential) => void, errorCallback: (e : Error) => void) {
+  let promise : Promise<UserCredential> | null = null;
+  switch (param.AuthType) {
+    case "EmailAndPassword":
+      promise = createUserWithEmailAndPassword(firebaseAuth, param.email as string, param.password as string);
+      break
+    case "GoogleAuth":
         const googleAuthProvider = new GoogleAuthProvider();
         googleAuthProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
         promise = signInWithPopup(firebaseAuth, googleAuthProvider);
         break;
-      case "TwitterAuth":
+    case "TwitterAuth":
         const twitterAuthProvider = new TwitterAuthProvider();
         promise = signInWithPopup(firebaseAuth, twitterAuthProvider);
         break;
-    }
-    if (promise == null ) {
-      const e = new Error("AuthType Not Found");
-      errorCallback(e);
-    } else {
-      promise
+  }
+  if (promise == null ) {
+    const e = new Error("AuthType Not Found");
+    errorCallback(e);
+  } else {
+    promise
       .then(result => {
-        updateProfile(result.user, { displayName: param.displayName ?? "" })
-        return result;
-      })
+          updateProfile(result.user, { displayName: param.displayName ?? "" })
+          return result;
+          })
+    .then(user => callback(user))
+      .catch(e => errorCallback(e));
+  }
+}
+function signin(param: AuthParameter, callback: (user: UserCredential) => void, errorCallback: (e : Error) => void){
+  let promise : Promise<UserCredential> | null = null;
+
+  switch (param.AuthType) {
+    case "Anonymously":
+      promise = signInAnonymously(firebaseAuth);
+      break;
+    case "EmailAndPassword":
+      promise = signInWithEmailAndPassword(firebaseAuth, param.email as string, param.password as string)
+        break;
+    case "GoogleAuth":
+      const googleAuthProvider = new GoogleAuthProvider();
+      googleAuthProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+      promise = signInWithPopup(firebaseAuth, googleAuthProvider);
+      break;
+    case "TwitterAuth":
+      const twitterAuthProvider = new TwitterAuthProvider();
+      promise = signInWithPopup(firebaseAuth, twitterAuthProvider);
+      break;
+  }
+  if (promise == null ) {
+    const e = new Error("AuthType Not Found");
+    errorCallback(e);
+  } else {
+    promise
       .then(user => callback(user))
       .catch(e => errorCallback(e));
-    }
-  };
+  }
+}
 
-  let signin = (param: AuthParameter, callback: (user: UserCredential) => void, errorCallback: (e : Error) => void) => {
-    let promise : Promise<UserCredential> | null = null;
-    
-    switch (param.AuthType) {
-      case "Anonymously":
-        promise = signInAnonymously(firebaseAuth);
-        break;
-      case "EmailAndPassword":
-        promise = signInWithEmailAndPassword(firebaseAuth, param.email as string, param.password as string)
-        break;
-      case "GoogleAuth":
-        const googleAuthProvider = new GoogleAuthProvider();
-        googleAuthProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-        promise = signInWithPopup(firebaseAuth, googleAuthProvider);
-        break;
-      case "TwitterAuth":
-        const twitterAuthProvider = new TwitterAuthProvider();
-        promise = signInWithPopup(firebaseAuth, twitterAuthProvider);
-        break;
-    }
-    if (promise == null ) {
-      const e = new Error("AuthType Not Found");
-      errorCallback(e);
-    } else {
-      promise
+function signout(callback: VoidFunction, errorCallback: (e : Error) => void) {
+  signOut(firebaseAuth)
+    .then(() => callback())
+    .catch(e => errorCallback(e));
+}
+
+function accountlink (param: AuthParameter, callback: (user: UserCredential) => void, errorCallback: (e : Error) => void) {
+  const authState = useAuthState();
+  if (!authState.user) {
+    const e = new Error("user not found");
+    errorCallback(e);
+  }
+
+  let promise : Promise<UserCredential> | null = null;
+
+  switch (param.AuthType) {
+    case "EmailAndPassword":
+      const c = EmailAuthProvider.credential(param.email as string, param.password as string);
+      promise = linkWithCredential(authState.user as User, c);
+      break;
+  }
+  if (promise == null ) {
+    const e = new Error("AuthType Not Found");
+    errorCallback(e);
+  } else {
+    promise
       .then(user => callback(user))
       .catch(e => errorCallback(e));
-    }
-  };
+  }
+}
 
-  let signout = (callback: VoidFunction, errorCallback: (e : Error) => void) => {
-      signOut(firebaseAuth)
-      .then(() => callback())
-      .catch(e => errorCallback(e));
-  };
-
-  let accountlink = (param: AuthParameter, callback: (user: UserCredential) => void, errorCallback: (e : Error) => void) => {
-    if (!authState.user) {
-      const e = new Error("user not found");
-      errorCallback(e);
-    }
-
-    let promise : Promise<UserCredential> | null = null;
-    
-    switch (param.AuthType) {
-      case "EmailAndPassword":
-        const c = EmailAuthProvider.credential(param.email as string, param.password as string);
-        promise = linkWithCredential(authState.user as User, c);
-        break;
-    }
-    if (promise == null ) {
-      const e = new Error("AuthType Not Found");
-      errorCallback(e);
-    } else {
-      promise
-      .then(user => callback(user))
-      .catch(e => errorCallback(e));
-    }
-  };
-
-  let value = { authState, signup, signin, signout, accountlink };
+function AuthProvider({ children }: { children: React.ReactNode }) {
+  const authState = useAuthState();
+  const value = { authState, signup, signin, signout, accountlink };
 
   return( 
     <AuthContext.Provider value={value}>
