e5f6870a83396c7a543d28aa89779e41cb4e3528
UCHIYAMA Ryota <lessironglance@gmail.com>
2023-05-03 22:51:43 +0900

    add routes

--- src/features/objectives/routes/KeyResult.tsx
+++ src/features/objectives/routes/KeyResult.tsx
@@ -1,4 +1,15 @@
 import { useParams } from 'react-router-dom';
 
 
+export const KeyResult = () => {
+  const { objectiveId } = useParams<"objectiveId">();
+  const { keyResultId } = useParams<"keyResultId">();
+
+  return (
+    <>
+      <h2>{objectiveId}</h2>
+      <h2>{keyResultId}</h2>
+    </>
+  );
+};
 
--- src/features/objectives/routes/index.tsx
+++ src/features/objectives/routes/index.tsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { NotFound } from '@/features/misc';
+
+import { KeyResult } from './KeyResult';
 import { Objective } from './Objective';
 import { Objectives } from './Objectives';
 
@@ -8,6 +11,8 @@ export const ObjectiveRoutes = () => {
     <Routes>
       <Route path="" element={<Objectives />} />
       <Route path=":objectiveId" element={<Objective />} />
+      <Route path=":objectiveId/key-results/:keyResultId" element={<KeyResult />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
