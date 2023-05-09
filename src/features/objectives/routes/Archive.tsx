ccae41647065c01fef5ac63dc81d196fb7a9628f
UCHIYAMA Ryota <lessironglance@gmail.com>
2023-05-10 00:17:00 +0900

    add toggle

--- src/features/objectives/routes/Archive.tsx
+++ src/features/objectives/routes/Archive.tsx
@@ -1,20 +1,99 @@
 import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useMutation, useQuery } from 'react-query';
 
 import { 
   Button, 
+  Box, 
   Container, 
+  Skeleton, 
   TextField, 
+  ToggleButton, 
   Typography, 
   } from '@mui/material';
 
+import { StyledToggleButtonGroup } from '@/components/ToggleButton';
 import { useAuth } from '@/hooks';
 import { MainContainerStyle } from '@/styles';
 
-import { getKeyResult, getObjective, setKeyResult } from '../api';
-import { InputKeyResult, KeyResultNotFound, ObjectiveNotFound } from '../components';
+import { 
+  getArchive, 
+  getKeyResults, 
+  getObjective, 
+  setArchive } from '../api';
+import { KeyResultCard, KeyResultNotFound, ObjectiveNotFound, RankRating } from '../components';
 import { createKeyResult } from '../functions';
+
+
+export const NewArchive = () => {
+  const { objectiveId } = useParams<"objectiveId">();
+  const navigate = useNavigate();
+  const authState = useAuth();
+
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const mode = searchParams.get("mode");
+
+  const { data: objective } = useQuery([ "objective", authState.user.uid, objectiveId], () => getObjective(authState.user.uid, objectiveId));
+  const { data: keyResults } = useQuery([ "key-results", authState.user.uid, objectiveId], () => getKeyResults(authState.user.uid, objectiveId));
+
+  //
+  const [formats, setFormats] = useState(() => []);
+    const handleFormat = (
+    event: React.MouseEvent<HTMLElement>,
+    newFormats: string[],
+  ) => {
+    setFormats(newFormats);
+  };
+  //
+
+
+  return (
+    <Container maxWidth="xl" sx={MainContainerStyle}>
+      <Typography variant="h3" noWrap component="h3">
+      {objectiveId}
+      </Typography>
+      <Typography variant="h3" noWrap component="h3">
+      {mode}
+      </Typography>
+
+      <Typography 
+        variant={"h3"}
+        component={"h3"} 
+        noWrap 
+        sx={{ flexGrow: 1, mx: 1, textAlign: "left"}}>
+        {"指標を選択してください"}
+      </Typography>
+      <StyledToggleButtonGroup
+      size="small"
+      color="primary"
+      orientation="vertical"
+      value={formats}
+      onChange={handleFormat}
+      sx={{ display: 'flex', width: '100%'}}
+      aria-label="text formatting">
+        { keyResults ?
+          keyResults.map((keyResult) => {
+            return(
+            <ToggleButton value={keyResult.id} key={keyResult.id}>
+              <Typography 
+                variant={"h3"}
+                component={"h3"} 
+                noWrap 
+                sx={{ flexGrow: 1, mx: 1, textAlign: "left"}}>
+                  {keyResult.title}
+              </Typography>
+              <RankRating sx={{mx: 1, flexGrow: 0 }} readOnly value={keyResult.rank} size="large"/>
+            </ToggleButton>
+            );
+            })
+            :
+            <Skeleton variant="rectangular" width={50} height={150} />
+            }
+      </StyledToggleButtonGroup>
+    </Container>
+      );
+};
 
 export const Archive = () => {
   const { objectiveId } = useParams<"objectiveId">();
@@ -26,6 +105,8 @@ export const Archive = () => {
     <Container maxWidth="xl" sx={MainContainerStyle}>
       <Typography variant="h3" noWrap component="h3">
       {objectiveId}
+      </Typography>
+      <Typography variant="h3" noWrap component="h3">
       {archiveId}
       </Typography>
     </Container>
--- src/features/objectives/routes/index.tsx
+++ src/features/objectives/routes/index.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import { NotFound } from '@/features/misc';
 
-import { Archive } from './Archive';
+import { Archive, NewArchive } from './Archive';
 import { KeyResult } from './KeyResult';
 import { Objective } from './Objective';
 import { Objectives } from './Objectives';
@@ -13,6 +13,7 @@ export const ObjectiveRoutes = () => {
       <Route path="" element={<Objectives />} />
       <Route path=":objectiveId" element={<Objective />} />
       <Route path=":objectiveId/key-results/:keyResultId" element={<KeyResult />} />
+      <Route path=":objectiveId/archives/new" element={<NewArchive />} />
       <Route path=":objectiveId/archives/:archiveId" element={<Archive />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
