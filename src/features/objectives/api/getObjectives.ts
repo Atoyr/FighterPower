c48bd169e44fc52be884c30e1d44e1f97a559fe5
UCHIYAMA Ryota <lessironglance@gmail.com>
2023-06-09 23:40:32 +0900

    update order by desc

--- src/features/objectives/api/getObjectives.ts
+++ src/features/objectives/api/getObjectives.ts
@@ -17,7 +17,7 @@ export const getObjectives = async (userId: string): Promise<Array<Objective>> =
   }
 
   const ref = collection(store, `users/${userId}/objectives`).withConverter(ObjectiveConverter);
-  const snapshot = await getDocs(query( ref, orderBy("modifiedAt")));
+  const snapshot = await getDocs(query( ref, orderBy("modifiedAt", "desc")));
   const objectives : Array<Objective> = [];
   snapshot.forEach((doc) => {
     objectives.push(doc.data());
